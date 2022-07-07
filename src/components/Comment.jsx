467c0f9c75d7869f2b5ebde3a4eb406fc4e4f0b0
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2022-07-07 15:12:58 -0400

    All the application developed in the modules: Starting with React; Application Structure; The React Engines. Completely done in javascript

---
+++ src/components/Comment.jsx
@@ -0,0 +1,53 @@
+import { Trash, ThumbsUp } from 'phosphor-react'
+import { useState } from 'react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar';
+
+export function Comment({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(state => {
+      return state + 1
+    })
+  }
+
+
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src='https://github.com/Lucas-B-C-Oliveira.png' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas B C Oliveira</strong>
+              <time title="11 de Maio ás 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1 hora atrás</time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+
+      </div>
+
+    </div>
+  )
+}
